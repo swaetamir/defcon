13cf46fdd7ee7a578c416b79a01c879d369cc7e6
swaetamir <swaeta.mobasher@gmail.com>
2026-04-15 20:34:15 -0400

    point video sources to cloudflare r2 cdn

--- lib/videos.ts
+++ lib/videos.ts
@@ -13,7 +13,7 @@ export const VIDEOS: Video[] = [
     title: 'BULLET FROM A GUN',
     artist: 'SKEPTA',
     year: 2019,
-    src: '/videos/bulletfromagun-skepta.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/bulletfromagun-skepta.mp4',
     thumbnail: '/thumbnails/bulletfromagun-skepta.jpg',
   },
   {
@@ -21,7 +21,7 @@ export const VIDEOS: Video[] = [
     title: 'X',
     artist: 'TIAKOLA',
     year: 2024,
-    src: '/videos/x-tiakola.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/x-tiakola.mp4',
     thumbnail: '/thumbnails/x-tiakola.jpg',
   },
   {
@@ -29,7 +29,7 @@ export const VIDEOS: Video[] = [
     title: 'COME N GO',
     artist: 'YEAT',
     year: 2025,
-    src: '/videos/comnego-yeat.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/comnego-yeat.mp4',
     thumbnail: '/thumbnails/comnego-yeat.jpg',
   },
   {
@@ -37,7 +37,7 @@ export const VIDEOS: Video[] = [
     title: '5AM IN TORONTO',
     artist: 'DRAKE',
     year: 2013,
-    src: '/videos/5amintoronto-drake.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/5amintoronto-drake.mp4',
     thumbnail: '/thumbnails/5amintoronto-drake.jpg',
   },
   {
@@ -45,7 +45,7 @@ export const VIDEOS: Video[] = [
     title: 'SKYBOX',
     artist: 'GUNNA',
     year: 2022,
-    src: '/videos/skybox-gunna.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/skybox-gunna.mp4',
     thumbnail: '/thumbnails/skybox-gunna.jpg',
   },
   {
@@ -53,7 +53,7 @@ export const VIDEOS: Video[] = [
     title: 'FASHION KILLA',
     artist: 'ASAP ROCKY',
     year: 2013,
-    src: '/videos/fashionkilla-asaprocky.mp4',
+    src: 'https://pub-0676fca3e3904780a0f8b5baf1c3b814.r2.dev/fashionkilla-asaprocky.mp4',
     thumbnail: '/thumbnails/fashionkilla-asaprocky.jpg',
   },
 ]
